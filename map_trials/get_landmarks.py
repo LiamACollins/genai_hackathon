ffeb5fee59b238aba4aaab6e94a46d83378d4bbb
Shyam Sankaran <shyamss@seas.upenn.edu>
2024-02-17 01:57:09 -0500

    tracking changes made

--- map_trials/get_landmarks.py
+++ map_trials/get_landmarks.py
@@ -8,12 +8,12 @@ def get_landmarks(latitude, longitude):
     api_key = os.environ["GOOGLEPLACES_API_KEY"]
     gmaps = googlemaps.Client(key=api_key)
     poi_results = gmaps.places_nearby(
-        location=user_location, radius=100, type="point_of_interest"
+        location=user_location, radius=1000, type="point_of_interest"
     )
 
     # Make a nearby search request for tourist attractions
     attraction_results = gmaps.places_nearby(
-        location=user_location, radius=100, type="tourist_attraction"
+        location=user_location, radius=1000, type="tourist_attraction"
     )
 
     return poi_results["results"], attraction_results["results"]
